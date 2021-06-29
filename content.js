9583a0b5ddddc194997f966cffa9484878791f46
jbr1gh7 <54848093+jbr1gh7@users.noreply.github.com>
2021-06-29 13:38:35 +0100

    bug fix and ui

--- content.js
+++ content.js
@@ -1,78 +1,76 @@
-const UTILITYFUNCTIONS = (function() {
-    let click = function(element) {
-        // Create our event (with options)
-        var event = new MouseEvent('click', {
-            bubbles: true,
-            cancelable: false,
-            view: window
+function click(element) {
+    // Create our event (with options)
+    var event = new MouseEvent('click', {
+        bubbles: true,
+        cancelable: false,
+        view: window
+    });
+};
+
+function handleContentRequests(message, sender, sendResponse) {
+    if (message.request === 'getDataFromContent') {
+        let url = window.location.href;
+        
+        //subject to frequent changes, dependant on exact structure of youtube webpage, could change at any moment
+        let constructTitleObject = function() {
+            let titleObject = {};
+
+            if (document.querySelectorAll('yt-formatted-string.style-scope.ytd-video-primary-info-renderer')[1] != undefined) {
+                titleObject.song = document.querySelectorAll('yt-formatted-string.style-scope.ytd-video-primary-info-renderer')[1].innerText;
+            }
+
+            if (document.querySelectorAll('a.yt-simple-endpoint.style-scope.yt-formatted-string')[0] != undefined) {
+                console.log('yes0')
+                titleObject.playlistWithSongPlaying = document.querySelectorAll('a.yt-simple-endpoint.style-scope.yt-formatted-string')[0].innerText;
+            }
+
+            if (document.querySelector('yt-formatted-string#text-displayed') != undefined) {
+                console.log('yes1');
+                titleObject.playlistNoSong = document.querySelector('yt-formatted-string#text-displayed').innerText;
+            }
+            else if (document.querySelector('h1#title.style-scope.ytd-playlist-sidebar-primary-info-renderer') != undefined) {
+                console.log('yes2');
+                titleObject.playlistNoSong = document.querySelector('h1#title.style-scope.ytd-playlist-sidebar-primary-info-renderer').innerText;
+            }
+
+            return titleObject;
+        };
+
+        sendResponse({
+            url: url,
+            titles: constructTitleObject()
         });
-    };
+    }
+    else if (message.request === 'injectFileDownload') {
+        console.log('uri:', message.uri);
 
-    return {
-        handleContentRequests: function(message, sender, sendResponse) {
-            if (message.request === 'getDataFromContent') {
-                let url = window.location.href;
-                
-                //subject to frequent changes, dependant on exact structure of youtube webpage, could change at any moment
-                let constructTitleObject = function() {
-                    let titleObject = {};
+        window.location.href = window.location.href;
 
-                    if (document.querySelectorAll('yt-formatted-string.style-scope.ytd-video-primary-info-renderer')[1] != undefined) {
-                        titleObject.song = document.querySelectorAll('yt-formatted-string.style-scope.ytd-video-primary-info-renderer')[1].innerText;
-                    }
+        let download = document.createElement('a');
+        download.id = 'song-getter-download';
+        download.setAttribute('href', message.uri);
+        download.setAttribute('download', message.fileName);
 
-                    if (document.querySelectorAll('a.yt-simple-endpoint.style-scope.yt-formatted-string')[0] != undefined) {
-                        console.log('yes0')
-                        titleObject.playlistWithSongPlaying = document.querySelectorAll('a.yt-simple-endpoint.style-scope.yt-formatted-string')[0].innerText;
-                    }
+        document.querySelector('body').appendChild(download);
+        let downloadInDoc = document.querySelector('#song-getter-download');
+        downloadInDoc.click();
+        downloadInDoc.remove();
 
-                    if (document.querySelector('yt-formatted-string#text-displayed') != undefined) {
-                        console.log('yes1');
-                        titleObject.playlistNoSong = document.querySelector('yt-formatted-string#text-displayed').innerText;
-                    }
-                    else if (document.querySelector('h1#title.style-scope.ytd-playlist-sidebar-primary-info-renderer') != undefined) {
-                        console.log('yes2');
-                        titleObject.playlistNoSong = document.querySelector('h1#title.style-scope.ytd-playlist-sidebar-primary-info-renderer').innerText;
-                    }
+        console.log('fileName: ', message.fileName);
+        chrome.runtime.sendMessage({
+            request: 'deleteDownloadedFile',
+            fileName: message.fileName
+        }, 
+        function(response) {
+            console.log('response: ', response);
+        }); 
 
-                    return titleObject;
-                };
-
-                sendResponse({
-                    url: url,
-                    titles: constructTitleObject()
-                });
-            }
-            else if (message.request === 'injectFileDownload') {
-                console.log('uri:', message.uri);
-
-                let download = document.createElement('a');
-                download.id = 'song-getter-download';
-                download.setAttribute('href', message.uri);
-                download.setAttribute('download', message.fileName);
-
-                document.querySelector('body').appendChild(download);
-                let downloadInDoc = document.querySelector('#song-getter-download');
-                downloadInDoc.click();
-                downloadInDoc.remove();
-
-                console.log('fileName: ', message.fileName);
-                chrome.runtime.sendMessage({
-                    request: 'deleteDownloadedFile',
-                    fileName: message.fileName
-                }, 
-                function(response) {
-                    console.log('response: ', response);
-                }); 
-
-                sendResponse({DataRecieved: message});
-            }
-        }
-    };
-}());
+        sendResponse({DataRecieved: message});
+    }
+}
 
 window.addEventListener('load', function() {
-    chrome.runtime.onMessage.addListener(UTILITYFUNCTIONS.handleContentRequests);
+    chrome.runtime.onMessage.addListener(handleContentRequests);
     console.log('element: ', document.querySelectorAll('h1#title.style-scope.ytd-playlist-sidebar-primary-info-renderer')); //playlist no song
 
 
--- popup.js
+++ popup.js
@@ -1,98 +1,88 @@
-const GETDATA = (function() {
-    return {
-        fetchVideoDataFromContent: function() {
-            return new Promise(function(resolve, reject) {
-                chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-                    chrome.tabs.sendMessage(tabs[0].id, {request: 'getDataFromContent'}, function(response) {
-                        console.log('Requesting data from content');
-                        console.log('response: ', response);
+function fetchVideoDataFromContent() {
+    return new Promise(function(resolve, reject) {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {request: 'getDataFromContent'}, function(response) {
+                console.log('Requesting data from content');
+                console.log('response: ', response);
+                songContainer = document.querySelector('#download-song-container');
+                songTitle = document.querySelector('#current-song-title');
+                playlistContainer = document.querySelector('#download-playlist-container');
+                playlistTitle = document.querySelector('#current-playlist-title');
 
-                        songContainer = document.querySelector('#download-song-container');
-                        songTitle = document.querySelector('#current-song-title');
+                let urlObject = {};
 
-                        playlistContainer = document.querySelector('#download-playlist-container');
-                        playlistTitle = document.querySelector('#current-playlist-title');
-        
-                        let urlObject = {};
-        
-                        if (response.url.includes('watch?v=') && response.url.includes('&list=')) {
-                            console.log('both');
+                if (response.url.includes('watch?v=') && response.url.includes('&list=')) {
+                    console.log('both');
+                    songTitle.innerText = response.titles.song;
+                    playlistTitle.innerText = response.titles.playlistWithSongPlaying;
+            
+                    songContainer.classList.remove('hidden');
+                    playlistContainer.classList.remove('hidden');
+            
+                    songContainer.classList.add('show-grid');
+                    playlistContainer.classList.add('show-grid');
+            
+                    urlObject.songUrl = response.url.split('?v=')[1];
+                    urlObject.playlistUrl = response.url.split('?list=')[1];
+                }
+                else if (response.url.includes('watch?v=')) {
+                    console.log('song in url');
+                    songTitle.innerText = response.titles.song;
 
-                            songTitle.innerText = response.titles.song;
-                            playlistTitle.innerText = response.titles.playlistWithSongPlaying;
-                    
-                            songContainer.classList.remove('hidden');
-                            playlistContainer.classList.remove('hidden');
-                    
-                            songContainer.classList.add('show-grid');
-                            playlistContainer.classList.add('show-grid');
-                    
-                            urlObject.songUrl = response.url.split('?v=')[1];
-                            urlObject.playlistUrl = response.url.split('?list=')[1];
-                        }
-                        else if (response.url.includes('watch?v=')) {
-                            console.log('song in url');
+                    songContainer.classList.remove('hidden');
+                    songContainer.classList.add('show-grid');
 
-                            songTitle.innerText = response.titles.song;
-        
-                            songContainer.classList.remove('hidden');
-                            songContainer.classList.add('show-grid');
-        
-                            urlObject.songUrl = response.url.split('?v=')[1];
-                        }
-                        else if (response.url.includes('playlist?list=')) {
-                            console.log('playlist in url');
+                    urlObject.songUrl = response.url.split('?v=')[1];
+                }
+                else if (response.url.includes('playlist?list=')) {
+                    console.log('playlist in url');
+                    playlistTitle.innerText = response.titles.playlistNoSong;
 
-                            playlistTitle.innerText = response.titles.playlistNoSong;
-        
-                            playlistContainer.classList.remove('hidden');
-                            playlistContainer.classList.add('show-grid');
-        
-                            urlObject.playlistUrl = response.url.split('?list=')[1];
-                        }
-                        else {reject('youtube URL not recognised as video or playlist')}
-        
-                        resolve(urlObject);
-                    });
-                });
+                    playlistContainer.classList.remove('hidden');
+                    playlistContainer.classList.add('show-grid');
+
+                    urlObject.playlistUrl = response.url.split('?list=')[1];
+                }
+                else {reject('youtube URL not recognised as video or playlist')}
+
+                resolve(urlObject);
             });
-        }
-    };
-}());
+        });
+    });
+}
 
-window.addEventListener('load', function() {
+function sendUrlToBackground(urlObject) {
+    console.log('sendUrl: ', urlObject);
+    let url = "";
+    let isPlaylist = false;
 
-    let sendUrlToBackground = function(event, urlObject) {
-        console.log('sendUrl');
-        let url = "";
-        let isPlaylist = false;
-    
-        if (urlObject.songUrl && urlObject.playlistUrl) {
-            if (event.target.id == 'get-song-button') {url = urlObject.songUrl}
-            else {
-                url = urlObject.playlistUrl;
-                isPlaylist = true;
-            }
-        }
-        else if (urlObject.songUrl && !urlObject.playlistUrl) {
-            url = urlObject.songUrl;
-        }
-        else if (!urlObject.songUrl && urlObject.playlistUrl) {
+    if (urlObject.songUrl && urlObject.playlistUrl) {
+        if (event.target.id == 'get-song-button') {url = urlObject.songUrl}
+        else {
             url = urlObject.playlistUrl;
             isPlaylist = true;
         }
-    
-        chrome.runtime.sendMessage({
-            request: 'postUrlToApi',
-            isPlaylist: isPlaylist,
-            url: url
-        });
-    };
+    }
+    else if (urlObject.songUrl && !urlObject.playlistUrl) {
+        url = urlObject.songUrl;
+    }
+    else if (!urlObject.songUrl && urlObject.playlistUrl) {
+        url = urlObject.playlistUrl;
+        isPlaylist = true;
+    }
 
-    GETDATA.fetchVideoDataFromContent().then(function(urlObject) {
+    chrome.runtime.sendMessage({
+        request: 'postUrlToApi',
+        isPlaylist: isPlaylist,
+        url: url
+    });
+};
+
+window.addEventListener('load', function() {
+    fetchVideoDataFromContent().then(function(urlObject) {
         console.log('url: ', urlObject);
-        document.querySelector('#get-song-button').addEventListener('click', sendUrlToBackground(event, urlObject));
-        document.querySelector('#get-playlist-button').addEventListener('click', sendUrlToBackground(event, urlObject));
+        document.querySelector('#get-song-button').addEventListener('click', function() {sendUrlToBackground(urlObject)});
+        document.querySelector('#get-playlist-button').addEventListener('click', function() {sendUrlToBackground(urlObject)});
     }).catch(function (/*error*/) {
         //console.warn(error);
     });
